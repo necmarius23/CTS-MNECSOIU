d3cad9ba534813f83db542422b939d56ce90793d
Marius Necsoiu <>
2021-03-02 14:33:15 +0200

    CTS-SEM1- added 2 custom exceptions, Profitable interface, Account

---
+++ seminar1/src/ro/ase/csie/cts/g1094/Main.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1094;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ seminar1/src/ro/ase/csie/cts/g1094/exceptions/InsufficientFundsException.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1094.exceptions;
+
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    public InsufficientFundsException() {
+
+    }
+
+}
