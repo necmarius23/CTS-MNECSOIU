8a873674aae2f4a7c481b944addc73f78b4ddd4a
Marius Necsoiu <>
2021-03-09 15:37:39 +0200

    FINAL COMMIT

---
+++ cts-seminar3/src/ro/ase/csie/cts/g1094/refactor/exceptions/InvalidAgeException.java
@@ -0,0 +1,4 @@
+package ro.ase.csie.cts.g1094.refactor.exceptions;
+
+public class InvalidAgeException extends Exception {
+}
--- cts-seminar3/src/ro/ase/csie/cts/g1094/refactor/phase1/Product.java
+++ cts-seminar3/src/ro/ase/csie/cts/g1094/refactor/phase1/Product.java
@@ -1,32 +1,56 @@
 package ro.ase.csie.cts.g1094.refactor.phase1;
 
 
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidAgeException;
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidPriceException;
+
 public class Product {
 
     public static final int MAX_AGE_ACCOUNT = 10;
     public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
 
-    public float computePriceWithDiscount(ProductType productType, float price, int accountAge)
+    public static float getDiscountValue(float price, float discount)
     {
-        float finalPrice = 0;
-        float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100;
-        switch(productType) {
-            case NEW:
-                finalPrice = price;
-                break;
-            case DISCOUNTED:
-                finalPrice = (price - (ProductType.DISCOUNTED.getDiscount() * price)) - fidelityDiscount * (price - (0.1f * price));
-                break;
-            case LIMITED_STOCK:
-                finalPrice = (price - (ProductType.LIMITED_STOCK.getDiscount() * price)) - fidelityDiscount * (price - (0.25f * price));
-                break;
-            case LEGACY:
-                finalPrice = (price - (ProductType.LEGACY.getDiscount() * price)) - fidelityDiscount * (price - (0.35f * price));
-                break;
-            default:
-                throw new UnsupportedOperationException("The enum type is not covered");
+        return discount * price;
+    }
 
+    private static float getPriceWithDiscountAndFidelity(
+            float price, float discountValue, float fidelityDiscount)
+    {
+        return (price - discountValue) * (1 - fidelityDiscount);
+    }
+
+    private static float getFidelityDiscount(int accountAge)
+    {
+        return (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100;
+    }
+    
+    private static float getFinalPrice(
+            float price, float fidelityDiscount, ProductType type)
+    {
+        float discountValue = getDiscountValue(price, type.getDiscount());
+        float finalPrice = getPriceWithDiscountAndFidelity(price, discountValue, fidelityDiscount);
+        return finalPrice;
+    }
+
+    public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeException
+    {
+
+        if (price <=0)
+        {
+            throw new InvalidPriceException();
         }
+        if (accountAge <0)
+        {
+            throw new InvalidAgeException();
+        }
+
+        float fidelityDiscount =
+                (productType == ProductType.NEW)? 0 : getFidelityDiscount(accountAge);
+
+        float finalPrice = getFinalPrice(price, fidelityDiscount, productType);
+
+
         return finalPrice;
     }
 }
