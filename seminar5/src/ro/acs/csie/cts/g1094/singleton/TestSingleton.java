832d22da6bf1c533981806a74a18e87f8834e9c4
Marius Necsoiu <>
2021-03-23 14:27:02 +0200

    Week 5

---
+++ seminar5/src/ro/acs/csie/cts/g1094/singleton/TestSingleton.java
@@ -0,0 +1,21 @@
+package ro.acs.csie.cts.g1094.singleton;
+
+public class TestSingleton {
+
+//    DbConnection con1 = new DbConnection();
+//    DbConnection con2 = new DbConnection();
+//
+//        DbConnection con1 = new DbConnection("127.0.0.1:3306","Db");
+//    DbConnection con2 = new DbConnection("127.0.0.1:3306","Db");
+//
+//
+//  DbConnection con1 = DbConnection.getInstance();
+//    DbConnection con2 = DbConnection.getInstance();
+
+    DbConnection con1 = DbConnection.getConnection("127.0.0.1:1443","cts");
+    DbConnection con2 = DbConnection.getConnection("127.0.0.1:1443","cts");
+//    if(con1 == con2){
+//        System.out.println("They are pointing to the same database");
+//    }
+
+}
